3c71232370e079963d290edf15dd45df908d3910
Fred Labanda <coderoph@gmail.com>
2020-04-17 22:49:57 +0800

    Init es6 module

---
+++ src/index.js
@@ -0,0 +1,25 @@
+module.exports = function camelize(o) {
+    let newO, origKey, newKey, value;
+    if (o instanceof Array) {
+        return o.map(val => {
+            if (typeof val === 'object') {
+                val = camelize(val);
+            }
+            return val;
+        });
+    } else {
+        newO = {};
+        for (origKey in o) {
+            if (o.hasOwnProperty(origKey)) {
+                newKey = (origKey.charAt(0).toLowerCase() + origKey.slice(1) || origKey).toString();
+                value = o[origKey];
+                if (value instanceof Array || (value !== null && value.constructor === Object)) {
+                    value = camelize(value);
+                }
+                newO[newKey] = value;
+            }
+        }
+    }
+
+    return newO;
+}
